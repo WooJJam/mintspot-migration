3161a4568327e81b01b018fd1f8b7f4bc7f165d4
WooJJam <jaemin5548@naver.com>
2024-02-08 01:44:40 +0900

    refactor: user 엔티티의 gender, sexual 변수 enum 으로 변경

---
+++ src/main/java/WooJJam/mintspot/domain/Sexual.java
@@ -0,0 +1,14 @@
+package WooJJam.mintspot.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Sexual {
+    HETEROSEXUAL("이성애자"),
+    BISEXUAL("양성애자"),
+    HOMOSEXUAL("동성애자");
+
+    private final String description;
+}
--- src/main/java/WooJJam/mintspot/domain/User.java
+++ src/main/java/WooJJam/mintspot/domain/User.java
@@ -1,9 +1,6 @@
 package WooJJam.mintspot.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 
 @Entity
@@ -16,10 +13,14 @@ public class User {
 
     private String email;
     private String password;
-    private String gender;
-    private String sexual;
 
-    public void createUser(String email, String password, String gender, String sexual) {
+    @Enumerated(value = EnumType.STRING)
+    private Gender gender;
+
+    @Enumerated(value = EnumType.STRING)
+    private Sexual sexual;
+
+    public void createUser(String email, String password, Gender gender, Sexual sexual) {
         this.email = email;
         this.password = password;
         this.gender = gender;
