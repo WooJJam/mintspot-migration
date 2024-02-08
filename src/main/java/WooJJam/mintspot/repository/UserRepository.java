4e8a817b3b755629ea75562757f3423484b9f260
WooJJam <jaemin5548@naver.com>
2024-02-09 02:34:56 +0900

    feat: findById와 findAll 기능 구현

--- src/main/java/WooJJam/mintspot/repository/UserRepository.java
+++ src/main/java/WooJJam/mintspot/repository/UserRepository.java
@@ -6,14 +6,26 @@ import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 @RequiredArgsConstructor
 public class UserRepository {
 
     private final EntityManager em;
 
+    public User findById(Long userId) {
+        return em.find(User.class, userId);
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User u", User.class)
+                .getResultList();
+    }
+
     public Long register(User user) {
         em.persist(user);
         return user.getId();
     }
+
 }
