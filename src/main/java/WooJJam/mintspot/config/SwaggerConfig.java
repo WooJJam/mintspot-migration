bc66c85b2231b785dca595570df702e1062e0243
WooJJam <jaemin5548@naver.com>
2024-04-06 02:06:51 +0900

    feat: swagger 세팅

---
+++ src/main/java/WooJJam/mintspot/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package WooJJam.mintspot.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// SwaggerConfig.java
+@OpenAPIDefinition(
+        info = @Info(title = "MINT SPOT",
+                description = "MINT SPOT API DESCRIPTION",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi chatOpenApi() {
+        String[] paths = {"/api/v1/**"};
+
+        return GroupedOpenApi.builder()
+                .group("재민의 스프링 부트 서버 세팅")
+                .pathsToMatch(paths)
+                .build();
+    }
+}
