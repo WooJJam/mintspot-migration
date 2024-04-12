a63e972645a98ea248d73ca9bd573960f0664c69
WooJJam <jaemin5548@naver.com>
2024-04-13 01:48:12 +0900

    feat: 채팅 화면 생성

--- src/main/java/WooJJam/mintspot/controller/mvc/ChatControllerMvc.java
+++ src/main/java/WooJJam/mintspot/controller/mvc/ChatControllerMvc.java
@@ -1,7 +1,9 @@
 package WooJJam.mintspot.controller.mvc;
 
+import WooJJam.mintspot.domain.chat.Category;
 import WooJJam.mintspot.domain.chat.Chat;
 import WooJJam.mintspot.domain.user.User;
+import WooJJam.mintspot.dto.chat.ChatCreateRequestBodyDto;
 import WooJJam.mintspot.repository.ChatRepository;
 import WooJJam.mintspot.repository.UserRepository;
 import WooJJam.mintspot.service.ChatGptService;
@@ -9,15 +11,14 @@ import WooJJam.mintspot.service.ChatService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @Controller
 @RequestMapping("/chat")
 @RequiredArgsConstructor
@@ -34,5 +35,25 @@ public class ChatControllerMvc {
         model.addAttribute("chats", chats);
         model.addAttribute("userId", userId);
         return "chat";
+    }
+
+    @GetMapping("/bot")
+    public String chat(Model model,
+                       @RequestParam("param") Category category,
+                       HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        String email = user.getEmail();
+        ChatCreateRequestBodyDto chatCreateRequestBodyDto = new ChatCreateRequestBodyDto(email, category);
+        chatService.createChat(chatCreateRequestBodyDto);
+        log.info("Chat Info = {}", chatCreateRequestBodyDto.toString());
+        return "chatBot";
+    }
+
+    @ResponseBody
+    @PostMapping("/bot")
+    public String bot() {
+        System.out.println("success");
+        return "success";
     }
 }
--- src/main/java/WooJJam/mintspot/dto/chat/ChatCreateRequestBodyDto.java
+++ src/main/java/WooJJam/mintspot/dto/chat/ChatCreateRequestBodyDto.java
@@ -1,12 +1,23 @@
 package WooJJam.mintspot.dto.chat;
 
 import WooJJam.mintspot.domain.chat.Category;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+@Data
 @Getter @Setter
 public class ChatCreateRequestBodyDto {
     private String email;
     private String title;
     private Category category;
+
+    public ChatCreateRequestBodyDto(String email, Category category) {
+        this.email = email;
+        this.category = category;
+        this.title = LocalDateTime.now().toString();
+    }
 }
